c042c3bdd814fcc0b9e011a214ff38000838349e
Lloyd Jayson Pintac <loyd_pintac@hotmail.com>
2019-07-10 22:48:37 -0500

    Automation first!!

--- E2ESeleniumJenkins/src/testcases/TC_001.java
+++ E2ESeleniumJenkins/src/testcases/TC_001.java
@@ -5,8 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class TC_001 {
-	
-	
+
 	@Test
 	public void testcase01() {
 		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
@@ -16,7 +15,7 @@ public class TC_001 {
 		driver.findElement(By.id("login_field")).sendKeys("loyd_pintac@yahoo.com");
 		driver.findElement(By.id("password")).sendKeys("EdithSerna143@");
 		driver.findElement(By.xpath("//*[@id=\"login\"]/form/div[3]/input[4]")).click();
-		//driver.quit();
+		// driver.quit();
 	}
 
 }
